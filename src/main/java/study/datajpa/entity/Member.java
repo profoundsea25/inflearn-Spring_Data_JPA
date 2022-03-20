dedf2f1d4fabf2285c0629f80720d4b0f2221ec2
profoundsea25 <profoundsea25@gmail.com>
2022-03-20 23:10:25 +0900

    예제 도메인 모델 ~15, 2022.03.20

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,27 +1,51 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     // private이 아닌 protected. JPA에서 활용할 때 프록시 객체 생성을 위해 열어두어야 한다.
-    protected Member() {
-    }
+    // Lombok 어노테이션으로 ㄷ대체
+//    protected Member() {
+//    }
+
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
